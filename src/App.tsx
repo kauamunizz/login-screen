213487a6539c2620886204e97ded8f5019371ba5
Kauã Muniz <104865007+kauamunizz@users.noreply.github.com>
2023-10-23 17:52:26 -0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import LoginScreen from './modules/login'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <LoginScreen />
+  )
+}
+
+export default App
---
+++ src/modules/login/screens/LoginScreen.tsx
@@ -0,0 +1,10 @@
+import { BackgroundImage } from "../styles/LoginScreen.styles";
+
+const LoginScreen = () => {
+
+    return (<div>
+        <BackgroundImage src="./background.jpg" />
+    </div>);
+};
+
+export default LoginScreen;
